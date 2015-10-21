702c7e3ffac91ab02404718ec4c09ff2ea2f3d86
Booker C. Bense <bbense@gmail.com>
2015-10-21 14:27:28 -0700

    wip

--- lib/volley_fire.ex
+++ lib/volley_fire.ex
@@ -15,18 +15,49 @@ defmodule VolleyFire do
   """
   def roll(task_list, count) do
     pid_list = start_all(task_list)
-    {start_now, rolling} = Enum.split(pid_list,count)
+    {start_now, rolling} = Enum.split(pid_list, count)
     Enum.map(start_now, &start(&1))
-
-
+    await(start_now, rolling)
   end
 
-  def start(pid) do
-
+  def start(%Task{pid: pid}) do
+    send pid, :start
   end
 
   def start_all(task_list) do
-
+    Enum.map(task_list, &wait_start(&1))
   end
+
+  def wait_start(task) do
+    wait_and_start = fn ->
+      receive do
+        :start -> task.()
+      end
+    end
+    Task.async(wait_and_start)
+  end
+
+  # Be careful, this will receive all messages sent
+  # to this process. It will return the first task
+  # reply and the list of tasks that came second.
+  def await(tasks,[]) do
+    tasks
+  end
+
+  def await(tasks, rolling) do
+    receive do
+      message ->
+        case Task.find(tasks, message) do
+          {_ , task} ->
+            List.delete(tasks, task)
+            [first | rest ] = rolling
+            start(first)
+            await(tasks ++ first, rest)
+        nil ->
+          await(tasks, rolling)
+        end
+    end
+  end
+
 
 end
